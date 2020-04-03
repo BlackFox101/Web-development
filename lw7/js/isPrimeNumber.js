3db6e8795cd2ed4677e2ae2421f4c7d82911830f
BlackFox101 <sad_alexxxei@@mail.ru>
2020-04-04 01:32:13 +0300

    isPrimeNumber

---
+++ lw7/js/isPrimeNumber.js
@@ -0,0 +1,50 @@
+function isPrimeNumber(number) {
+  isPrime = true
+  if (!Array.isArray(number)) {
+    if (Number.isInteger(number)) {
+      if (number > 1) {
+        for (let j = 2; j < number; j++) {
+          if (number % j == 0) {
+            isPrime = false
+            break
+          }
+        }
+      } else {
+        isPrime = false
+      }
+
+      if (isPrime) {
+        console.log('The number', number,'is prime')
+      }
+      else {
+        console.log('The number', number,'is not prime')
+      }
+    } else {
+      console.log('Не верные данные!')
+    }
+
+  } else {
+
+    for (var i = 0; i < number.length; i++) {
+      isPrime = true
+      if (number[i] > 1) {
+        for (let j = 2; j < number[i]; j++) {
+          if (number[i] % j == 0) {
+            isPrime = false
+            break
+          }
+        }
+
+      } else {
+        isPrime = false
+      }
+
+      if (isPrime) {
+        console.log('The number', number[i],'is prime')
+      }
+      else {
+        console.log('The number', number[i],'is not prime')
+      }
+    }
+  }
+}
