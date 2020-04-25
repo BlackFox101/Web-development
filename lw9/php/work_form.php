b7eaba19f70814e543e728d939c9eb73a981dc18
BlackFox101 <sad_alexxxei@@mail.ru>
2020-04-25 17:40:34 +0300

    Update work_form.php

--- lw9/php/work_form.php
+++ lw9/php/work_form.php
@@ -1,57 +1,57 @@
 <?php
-$first_name = $_POST['first_name'];
-$email = $_POST['email'];
-if (preg_match("/^[a-zA-Z]+$/",$first_name))
-{
-  $correct_name = 'true';
-}
-elseif (preg_match("/^[А-Яа-яЁё]+$/",$first_name))
-{
-  $correct_name = 'true';
-}
-else
-{
-  $correct_name = 'false';
-}
-
-$direction = '../';
-chdir($direction);
-$direction = 'data/' . $email . '.txt';
-
-if (filter_var($email, FILTER_VALIDATE_EMAIL))
-{
-  if (!file_exists($direction))
+  $first_name = $_POST['first_name'];
+  $email = $_POST['email'];
+  if (preg_match("/^[a-zA-Z]+$/",$first_name))
   {
-    $correct_email = 'true';
+    $correct_name = 'true';
+  }
+  elseif (preg_match("/^[А-Яа-яЁё]+$/",$first_name))
+  {
+    $correct_name = 'true';
   }
   else
   {
-    $correct_email = 'registered';
+    $correct_name = 'false';
   }
-}
-else
-{
-  $correct_email = 'false';
-}
 
-$my_array = ["email" => "$correct_email",
-             "first_name" => "$correct_name"];
-$json_str = json_encode($my_array);
-echo $json_str;
+  $direction = '../';
+  chdir($direction);
+  $direction = 'data/' . $email . '.txt';
 
-if ($correct_email == 'true' && $correct_name == 'true')
-{
-  form_entry($direction, $email);
-}
+  if (filter_var($email, FILTER_VALIDATE_EMAIL))
+  {
+    if (!file_exists($direction))
+    {
+      $correct_email = 'true';
+    }
+    else
+    {
+      $correct_email = 'registered';
+    }
+  }
+  else
+  {
+    $correct_email = 'false';
+  }
 
-function form_entry($direction, $email)
-{
-  $first_name = $_POST['first_name'];
-  $country = $_POST['country'];
-  $gender = $_POST['gender'];
-  $message = $_POST['message'];
+  $my_array = ["email" => "$correct_email",
+               "first_name" => "$correct_name"];
+  $json_str = json_encode($my_array);
+  echo $json_str;
 
-  $fd = fopen($direction, 'w+');
-  fwrite($fd, $first_name."\r\n".$email."\r\n".$country."\r\n".$gender."\r\n".$message);
-  fclose($fd);
-}
+  if ($correct_email == 'true' && $correct_name == 'true')
+  {
+    form_entry($direction, $email);
+  }
+
+  function form_entry($direction, $email)
+  {
+    $first_name = $_POST['first_name'];
+    $country = $_POST['country'];
+    $gender = $_POST['gender'];
+    $message = $_POST['message'];
+
+    $fd = fopen($direction, 'w+');
+    fwrite($fd, $first_name."\r\n".$email."\r\n".$country."\r\n".$gender."\r\n".$message);
+    fclose($fd);
+  }
